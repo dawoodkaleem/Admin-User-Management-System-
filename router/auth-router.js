bc71a458551fd6143d8c0ce89eee31ae3cbb7623
dawoodkaleem <dawood9743@gmail.com>
2024-12-06 17:24:14 +0500

    Db console removed and  login validation added in auth router

--- router/auth-router.js
+++ router/auth-router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authcontrollers = require("../controllers/auth-controller");
 const signupSchema = require("../validators/auth-validtor");
 const validate = require("../midderwares/validate-middleware");
+const { loginSchema } = require("../validators/login-validtor");
 // app.use();
 
 // router.get("/", (req, res) => {
@@ -13,6 +14,6 @@ router.route("/").get(authcontrollers.home);
 router
   .route("/register")
   .post(validate(signupSchema), authcontrollers.register);
-router.route("/login").post(authcontrollers.login);
+router.route("/login").post(validate(loginSchema), authcontrollers.login);
 
 module.exports = router;
--- utils/db.js
+++ utils/db.js
@@ -1,17 +1,7 @@
 const mongoose = require("mongoose");
-// const URI = "mongodb://127.0.0.1:27017/mern_admin";
-// const URI =
-//   "mongodb+srv://dawood9743:tMnIlsa9VcBtRevG@cluster0.yrnmq.mongodb.net/mern_admin?retryWrites=true&w=majority&appName=Cluster0";
+
 const URI = process.env.MONGODB_URI;
-// const URI =
-//   "mongodb+srv://dawood9743:tMnIlsa9VcBtRevG@cluster0.yrnmq.mongodb.net/mern_admin";
-// const URI =
-//   "mongodb+srv://dawood9743:tMnIlsa9VcBtRevG@cluster0.yrnmq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// const URI =
-//   "mongodb+srv://dawood9743:damon%40123@cluster0.fwik4.mongodb.net/mern_admin?retryWrites=true&w=majority&appName=Cluster0";
-
-// mongoose.connect(URI);
 const connectDb = async () => {
   try {
     await mongoose.connect(URI);
