342627c9705da86cf6cd773a5267fbac8c7bc6e5
dawoodkaleem <dawood9743@gmail.com>
2024-12-20 10:32:31 +0500

    Contacts of all users

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,9 @@
+const { json } = require("express");
 const User = require("../models/user-model");
+const Contact = require("../models/contact-model");
+// ____________________________
+//  Get All Users Data
+// ____________________________
 
 const getAllUsers = async (req, res, next) => {
   try {
@@ -12,5 +17,20 @@ const getAllUsers = async (req, res, next) => {
     next(error);
   }
 };
+// ____________________________
+//  Get All Users Contacts Data
+// ____________________________
 
-module.exports = getAllUsers;
+const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    return res.status(200).json(contacts);
+    if (constacts || contacts.length === 0) {
+      return res.status(404).json({ message: "No Contact Found" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getAllUsers, getAllContacts };
