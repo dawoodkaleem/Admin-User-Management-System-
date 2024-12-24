c73d54dd82b4ea443693b8b13c5db8d9694ee77f
dawoodkaleem <dawood9743@gmail.com>
2024-12-24 14:29:23 +0500

    admin verification if loging or not users

--- router/admin-router.js
+++ router/admin-router.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const adminController = require("../controllers/admin-controller");
 const router = express.Router();
+const authMiddleware = require("../midderwares/auth-middleware");
 
-router.route("/users").get(adminController.getAllUsers);
+router.route("/users").get(authMiddleware, adminController.getAllUsers);
 router.route("/contacts").get(adminController.getAllContacts);
 
 module.exports = router;
