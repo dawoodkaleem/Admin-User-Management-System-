9d17814a92592c0adb9ce47d09beca1b07b3f575
dawoodkaleem <dawood9743@gmail.com>
2024-12-24 14:57:32 +0500

    Checking is Admin or not

--- midderwares/admin-middleware.js
+++ midderwares/admin-middleware.js
@@ -1,6 +1,12 @@
 const adminMiddleware = async (req, res, next) => {
   try {
-    console.log(req.user);
+    console.log(req.user.isAdmin);
+    const adminRole = req.user.isAdmin;
+    if (!adminRole) {
+      res.status(403).json({ message: "Access denied .User is not an admin." });
+    }
+
+    next();
   } catch (error) {
     next(error);
   }
--- router/admin-router.js
+++ router/admin-router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const adminController = require("../controllers/admin-controller");
 const router = express.Router();
 const authMiddleware = require("../midderwares/auth-middleware");
-
+const adminMiddleware = require("../midderwares/admin-middleware");
 router
   .route("/users")
   .get(authMiddleware, adminMiddleware, adminController.getAllUsers);
