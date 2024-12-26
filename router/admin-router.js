0dae1372991844015e8a9c26b4f43aab5d678afb
dawoodkaleem <dawood9743@gmail.com>
2024-12-26 21:21:16 +0500

    Error resolved

--- router/admin-router.js
+++ router/admin-router.js
@@ -3,12 +3,13 @@ const adminController = require("../controllers/admin-controller");
 const router = express.Router();
 const authMiddleware = require("../midderwares/auth-middleware");
 const adminMiddleware = require("../midderwares/admin-middleware");
+// const deleteUserById = require("../midderwares/admin-middleware");
 router
   .route("/users")
   .get(authMiddleware, adminMiddleware, adminController.getAllUsers);
 router
   .route("/users/delete/:id")
-  .delete(authMiddleware, adminMiddleware.adminController.deleteUserById);
+  .delete(authMiddleware, adminMiddleware, adminController.deleteUserById);
 router
   .route("/contacts")
   .get(authMiddleware, adminMiddleware, adminController.getAllContacts);
